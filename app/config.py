94a2bb5b38cfc6d2c4f42a0e1fb540050079dfb3
nabilmuafa <nmuafa@gmail.com>
2025-05-09 18:37:02 +0700

    fix app config & Dockerfile

--- app/config.py
+++ app/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Elasticsearch connection
-ELASTIC_HOST = f"http://localhost:{os.getenv('ES_PORT', 9200)}"
+ELASTIC_HOST = f"http://es:{os.getenv('ES_PORT', 9200)}"
 ELASTIC_USER = "elastic"
-ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD", "123456")
+ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD", "changeme")
 
 # Index name default
 ELASTIC_INDEX = os.getenv("ELASTIC_INDEX", "scifact")
