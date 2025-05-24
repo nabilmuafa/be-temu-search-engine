256cfcbbac4d16dc2a90b54367aa6a0abafb3f1c
nabilmuafa <109450293+nabilmuafa@users.noreply.github.com>
2025-05-24 15:22:18 +0700

    fix es host & add caddy to dockerfile

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Elasticsearch connection
-# ELASTIC_HOST = f"http://es:{os.getenv('ES_PORT', 9200)}"
-ELASTIC_HOST = f"http://localhost:{os.getenv('ES_PORT', 9200)}"
+ELASTIC_HOST = f"http://es:{os.getenv('ES_PORT', 9200)}"
+# ELASTIC_HOST = f"http://localhost:{os.getenv('ES_PORT', 9200)}"
 ELASTIC_USER = "elastic"
 ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD", "changeme")
 
